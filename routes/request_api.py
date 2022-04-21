bb1a95c4ebc34c6174038b9bb36c6dbeb990ff65
Sourcery AI <>
2022-04-21 12:59:54 +0000

    'Refactored by Sourcery'

--- routes/request_api.py
+++ routes/request_api.py
@@ -16,13 +16,13 @@ BOOK_REQUESTS = {
     "8c36e86c-13b9-4102-a44f-646015dfd981": {
         'title': u'Good Book',
         'email': u'testuser1@test.com',
-        'timestamp': (datetime.today() - timedelta(1)).timestamp()
+        'timestamp': (datetime.now() - timedelta(1)).timestamp(),
     },
     "04cfc704-acb2-40af-a8d3-4611fab54ada": {
         'title': u'Bad Book',
         'email': u'testuser2@test.com',
-        'timestamp': (datetime.today() - timedelta(2)).timestamp()
-    }
+        'timestamp': (datetime.now() - timedelta(2)).timestamp(),
+    },
 }
 
 
